99a28676b2f435e2919979968518d42e049d9938
M4rk9696 <nimalan.m@protonmail.com>
2020-04-12 22:19:18 +0530

    Create client code

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-var css = require('sheetify')
-var choo = require('choo')
+const html = require('nanohtml')
+const css = require("sheetify")
 
-css('tachyons')
+css("tachyons")
 
-var app = choo()
-if (process.env.NODE_ENV !== 'production') {
-  app.use(require('choo-devtools')())
-} else {
-  app.use(require('choo-service-worker')())
-}
+const el = html`
+  <body>
+    <div class="pa4 black-80">
+      <form class="measure center" action=${`http://192.168.68.114:8080/v1/add`} method="POST">
+        <fieldset class="ba b--transparent ph0 mh0">
+          <legend class="f4 fw6 ph0 mh0">Add bookmark</legend>
+          <div class="mt3">
+            <label class="db fw6 lh-copy f6" for="url">URL: </label>
+            <input class="pa2 input-reset ba bg-transparent w-100" id="url" name="url" type="text">
+          </label>
+          <div class="mt3">
+            <label class="db fw6 lh-copy f6" for="desc">Description: </label>
+            <input class="pa2 input-reset ba bg-transparent w-100" id="desc" name="desc" type="text">
+          </div>
+          <div class="mt3">
+            <label class="db fw6 lh-copy f6" for="tags">Tags(Comma Seperated): </label>
+            <input class="pa2 input-reset ba bg-transparent w-100" id="tags" name="tags" type="text">
+          </div class="mt3">
+        </fieldset>
+        <div class="">
+          <input class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Submit">
+        </div>
+      </form>
+    </div>
+  </body>
+`
 
-app.use(require('./stores/clicks'))
-
-app.route('/', require('./views/main'))
-app.route('/*', require('./views/404'))
-
-module.exports = app.mount('body')
+document.body.appendChild(el)
